68d0a932f8a18870e8de78ac5ed98b2a794e8fe0
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-12-06 21:53:17 +0500

    .

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,9 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Muhammadtalha678.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+ url: 'https://muhammadtalha678.github.io',
+baseUrl: '/ai-driven-hackathon/',
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
